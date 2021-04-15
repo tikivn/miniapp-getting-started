922d279938046be497b995152309b24f2c20751d
Han Han <han.han@tiki.vn>
2021-04-15 11:18:44 +0700

    Add payment flow

---
+++ shopping/src/services/index.js
@@ -0,0 +1,27 @@
+const BASE_URL = "https://api.tala.xyz/miniapp/api/graphql/query";
+export default async function makeGraphqlRequest(
+  { query, variables },
+  token = ""
+) {
+  return new Promise((resolve, reject) => {
+    try {
+      my.request({
+        url: BASE_URL,
+        data: { query, variables },
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "X-Tiki-Access-Token": token,
+        },
+        success: (res) => {
+          resolve(res);
+        },
+        fail(e) {
+          reject(e);
+        },
+      });
+    } catch (error) {
+      reject(error);
+    }
+  });
+}
