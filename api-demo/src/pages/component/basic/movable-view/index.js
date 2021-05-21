999acb90b594eff8cebd2eeb0885a9a630cc026b
Viet Nguyen <ho@ngviet.com>
2021-05-21 23:27:39 +0700

    adds movable-view demo

---
+++ api-demo/src/pages/component/basic/movable-view/index.js
@@ -0,0 +1,14 @@
+Page({
+  onTouchStart(e) {
+    console.log('---------', e);
+  },
+  onTouchMove(e) {
+    console.log('---------', e);
+  },
+  onTouchEnd(e) {
+    console.log('---------', e);
+  },
+  onTouchCancel(e) {
+    console.log('---------', e);
+  },
+})
