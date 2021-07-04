2c0e8d4c4c22fe4cf9bd32b627ab7a9e0bd6fbce
hungnguyentiki <hung.nguyen7@tiki.vn>
2021-07-04 18:52:20 +0700

    fix home

--- shopping/src/pages/index/index.js
+++ shopping/src/pages/index/index.js
@@ -1,1 +1,13 @@
-Page({});
+Page({
+  onPageScroll(event) {
+    if (event.scrollTop > 120) {
+      my.setNavigationBar({
+        title: "Ngày hội đọc sách",
+      });
+    } else {
+      my.setNavigationBar({
+        title: "  ",
+      });
+    }
+  },
+});
