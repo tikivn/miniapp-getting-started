8c6b7f8206f9d1c2ce4c81efea57c98570083851
Kien Nguyen <kien.nguyen3@tiki.vn>
2021-11-16 17:48:30 +0700

    add redirect to API

--- api-demo/src/pages/api/navigator/index.js
+++ api-demo/src/pages/api/navigator/index.js
@@ -1,13 +1,19 @@
 Page({
   switchTab() {
     my.switchTab({
-      url: "pages/api/index"
-    })
+      url: 'pages/api/index',
+    });
   },
 
   navigateTo() {
     my.navigateTo({
-      url: "pages/api/navigator/navigator-lvl2/index",
+      url: 'pages/api/navigator/navigator-lvl2/index',
+    });
+  },
+
+  redirectTo() {
+    my.redirectTo({
+      url: 'pages/api/navigator/navigator-lvl2/index',
     });
   },
 
@@ -17,17 +23,16 @@ Page({
 
   navigateToMiniApp() {
     my.navigateToMiniApp({
-      appId: "xyz.tala.test.miniappone",
-      path: "pages/api/navigator/index",
+      appId: 'xyz.tala.test.miniappone',
+      path: 'pages/api/navigator/index',
       appMeta: {
         cdnBaseUrl:
-          "https://dev-tikiscp.tbox.vn/miniapps/xyz.tala.test.miniappone/1.1.0/2/151feea1818111eb86082e3a2f899608/",
-        frameworkVersion: "1.34.0",
-        frameworkCdnBaseDomain:
-          "https://dev-tikiscp.tbox.vn/miniapps/framework_files",
+          'https://dev-tikiscp.tbox.vn/miniapps/xyz.tala.test.miniappone/1.1.0/2/151feea1818111eb86082e3a2f899608/',
+        frameworkVersion: '1.34.0',
+        frameworkCdnBaseDomain: 'https://dev-tikiscp.tbox.vn/miniapps/framework_files',
       },
       extraData: {
-        from: "MiniApp Demo",
+        from: 'MiniApp Demo',
       },
     });
   },
@@ -37,6 +42,6 @@ Page({
   },
 
   relaunch() {
-    my.reLaunch({ url: "pages/api/confirm/index" });
+    my.reLaunch({ url: 'pages/api/confirm/index' });
   },
 });
