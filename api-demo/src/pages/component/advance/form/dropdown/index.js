957820c4f564f2da1999adb6a91324c99adbf8dd
hungnguyentiki <hung.nguyen7@tiki.vn>
2021-12-08 14:22:23 +0700

    chore: revert dropdown

--- api-demo/src/pages/component/advance/form/dropdown/index.js
+++ api-demo/src/pages/component/advance/form/dropdown/index.js
@@ -1,5 +1,17 @@
 Page({
   data: {
     items1: ['Item 1', 'Item 2', 'Item 3'],
+    items2: [
+      { key: 1, name: 'Item 1' },
+      { key: 2, name: 'Item 2' },
+    ],
+    item1: 'Item 1',
+    item2: null,
+  },
+  onSelect1(item1) {
+    this.setData({ item1 });
+  },
+  onSelect2(item2) {
+    this.setData({ item2 });
   },
 });
