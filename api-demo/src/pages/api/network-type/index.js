e0f005f3015d7f5663c9a39808b503552363602c
Tai Than <tai.than@tiki.vn>
2021-12-14 20:55:07 +0700

    feat(core): add jsapi getNetworkType

---
+++ api-demo/src/pages/api/network-type/index.js
@@ -0,0 +1,17 @@
+Page({
+  data: {
+    networkType: undefined,
+  },
+  onGetNetworkType() {
+    my.getNetworkType({
+      success: (res) => {
+        this.setData({ networkType: res });
+        my.alert({ title: 'Success', content: JSON.stringify(res) });
+      },
+      fail: (e) => {
+        console.log(e);
+        my.alert({ title: 'Fail', content: JSON.stringify(e) });
+      },
+    });
+  },
+});
