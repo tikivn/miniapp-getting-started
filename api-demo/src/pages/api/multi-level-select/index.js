281233de978a6968ccb5fe867f2ebf75e862f7fd
Dang Pham <dang.pham@tiki.vn>
2021-12-17 22:02:57 +0700

    update

--- api-demo/src/pages/api/multi-level-select/index.js
+++ api-demo/src/pages/api/multi-level-select/index.js
@@ -2,18 +2,64 @@ Page({
   onShowMultiLevelSelect() {
     if (my.canIUse('multiLevelSelect')) {
       my.multiLevelSelect({
-        title: 'Title',
-        optionsOne: [
-          'Every Monday',
-          'Every Tuesday',
-          'Every Wednesday',
-          'Every Thursday',
-          'Every Friday',
-          'Every Saturday',
-          'Every Sunday',
+        title: 'Place of birth',
+        suggest: 'Please chosse',
+        list: [
+          {
+            name: 'Hangzhou City',
+            subList: [
+              {
+                name: 'West Lake District',
+                subList: [
+                  {
+                    name: 'Gucui Street',
+                  },
+                  {
+                    name: 'Wenxin Street',
+                  },
+                ],
+              },
+              {
+                name: 'Uptown',
+                subList: [
+                  {
+                    name: "Yan'an Street",
+                  },
+                  {
+                    name: 'Longxiangqiao Street',
+                    subList: [
+                      {
+                        name: 'Longxiangqiao Street 1',
+                      },
+                      {
+                        name: 'Longxiangqiao Street 2',
+                      },
+                    ],
+                  },
+                ],
+              },
+            ],
+          },
+          {
+            name: 'Shanghai City', 
+          },
+          {
+            name: 'HCM City', 
+            subList: [
+              {
+                name: "District 1",
+              },
+              {
+                name: 'District 3',
+              },
+            ],
+          },
         ],
-        selectedOneIndex: 2,
-        success: (res) => {},
+        success: (res) => {
+          my.alert({
+            title: `Result: ${JSON.stringify(res)}`,
+          });
+        },
       });
     }
   },
