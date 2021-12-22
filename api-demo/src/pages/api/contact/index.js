0ff0ec7c7ebb0042dc2ffd2da7f66ec4cb9f21ef
mai.nguyen-tiki <mai.nguyen6@tiki.vn>
2021-12-22 15:53:21 +0700

    feat(core): add jsapi addPhoneContact

--- api-demo/src/pages/api/contact/index.js
+++ api-demo/src/pages/api/contact/index.js
@@ -1,4 +1,51 @@
 Page({
+  data: {
+    photoFilePath: '/sdcard/DCIM/Camera/a.jpg',
+    nickName: 'tinniapp',
+    lastName: 'Last',
+    middleName: 'Middle',
+    firstName: 'First',
+    remark: 'this is remark',
+    mobilePhoneNumber: '13800000000',
+    homePhoneNumber: '11111115',
+    workPhoneNumber: '11111112',
+    homeFaxNumber: '11111114',
+    workFaxNumber: '11111111',
+    hostNumber: '11111113',
+    weChatNumber: 'liuhuo',
+    alipayAccount: 'alipay@alipay.com',
+    addressCountry: 'US',
+    addressState: 'California',
+    addressCity: 'San Francisco',
+    addressStreet: 'Mountain View',
+    addressPostalCode: '94016',
+    workAddressCountry: 'China',
+    workAddressState: 'Zhejiang',
+    workAddressCity: 'Hangzhou',
+    workAddressStreet: 'Tianmushan Road',
+    workAddressPostalCode: '361005',
+    homeAddressCountry: 'Canada',
+    homeAddressState: 'Ontairo',
+    homeAddressCity: 'Toronto',
+    homeAddressStreet: 'No.234 Road',
+    homeAddressPostalCode: '123456',
+    organization: 'AntFin',
+    title: 'Developer',
+    email: 'liuhuo01@sina.com',
+    url: 'www.alipay.com',
+    success: (res) => {
+      my.alert({
+        title:"Success",
+        content: 'addPhoneContact response: ' + JSON.stringify(res),
+      });
+    },
+    fail: (res) => {
+      my.alert({
+        title:"Fail",
+        content: 'addPhoneContact response: ' + JSON.stringify(res),
+      });
+    },
+  },
   alert(message) {
     my.alert({
       title: 'response',
@@ -6,6 +53,11 @@ Page({
       buttonText: 'OK',
     });
   },
+
+  onInput(e) {
+    this.data[e.currentTarget.id] = e.detail.value;
+  },
+  
   choosePhoneContact() {
     my.choosePhoneContact({
       success: (success) => {
@@ -15,5 +67,10 @@ Page({
         this.alert(JSON.stringify(fail));
       },
     });
+  },
+  addPhoneContact() {
+    if (my.canIUse('addPhoneContact')) {
+      my.addPhoneContact(this.data);
+    }
   },
 });
