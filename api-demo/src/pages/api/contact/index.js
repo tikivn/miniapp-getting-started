96cc1c89543b30fb0af902540025bb2d30ed3581
mainguyen-tiki <55970585+mainguyen-tiki@users.noreply.github.com>
2022-02-23 13:43:02 +0700

    feat(core): jsapi get all contact (#76)
    * feat(core): add jsapi addPhoneContact
    
    * feat: doc jsapi my.addPhonecontact
    
    * feat(core): add get all contacts

--- api-demo/src/pages/api/contact/index.js
+++ api-demo/src/pages/api/contact/index.js
@@ -1,4 +1,51 @@
 Page({
+  data: {
+    photoFilePath: '/sdcard/DCIM/Camera/a.jpg',
+    nickName: 'tinniapp',
+    lastName: 'Last',
+    middleName: 'Middle',
+    firstName: 'First',
+    remark: 'remark',
+    mobilePhoneNumber: '13800000000',
+    homePhoneNumber: '11111115',
+    workPhoneNumber: '11111112',
+    homeFaxNumber: '11111114',
+    workFaxNumber: '11111111',
+    hostNumber: '11111113',
+    weChatNumber: 'liuhuo',
+    alipayAccount: 'tiki.vn',
+    addressCountry: 'Viet Name',
+    addressState: 'Q.10',
+    addressCity: 'HCM',
+    addressStreet: 'CMT8',
+    addressPostalCode: '94016',
+    workAddressCountry: 'Viet Name',
+    workAddressState: 'Q.10',
+    workAddressCity: 'HCM',
+    workAddressStreet:  'CMT8',
+    workAddressPostalCode: '361005',
+    homeAddressCountry: 'Viet Name',
+    homeAddressState: 'Binh Duong',
+    homeAddressCity: 'Thuan An',
+    homeAddressStreet: 'Nguyen Huu Canh',
+    homeAddressPostalCode: '123456',
+    organization: 'Tiki',
+    title: 'Developer',
+    email: 'liuhuo01@tiki.vn',
+    url: 'www.tiki.vn',
+    success: (res) => {
+      my.alert({
+        title:"Success",
+        content: 'addPhoneContact response: ' + JSON.stringify(res),
+      });
+    },
+    fail: (res) => {
+      my.alert({
+        title:"Fail",
+        content: 'addPhoneContact response: ' + JSON.stringify(res),
+      });
+    },
+  },
   alert(message) {
     my.alert({
       title: 'response',
@@ -6,6 +53,11 @@ Page({
       buttonText: 'OK',
     });
   },
+
+  onInput(e) {
+    this.data[e.currentTarget.id] = e.detail.value;
+  },
+  
   choosePhoneContact() {
     my.choosePhoneContact({
       success: (success) => {
@@ -16,4 +68,21 @@ Page({
       },
     });
   },
+  addPhoneContact() {
+    if (my.canIUse('addPhoneContact')) {
+      my.addPhoneContact(this.data);
+    }
+  },
+
+  getAllContacts(){
+    my.getAllContacts({
+      scope:["phone"],
+      success: (success) => {
+        this.alert(JSON.stringify(success));
+      },
+      fail: (fail) => {
+        this.alert(JSON.stringify(fail));
+      },
+    });
+  }
 });
