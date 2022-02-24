70e81f4712c8668aa2f46e474e0cce711623fa41
phamdang2810-tiki <dang.pham@tiki.vn>
2022-02-24 15:34:30 +0700

    update

--- api-demo/src/pages/api/acceleronmeter/index.js
+++ api-demo/src/pages/api/acceleronmeter/index.js
@@ -1,7 +1,11 @@
 Page({
+  data: {
+    data: {x: 0, y: 0, z: 0},
+  },
   onReady() {
     this.acceleronmeterChangeCallback = (res) => {
-      my.alert({title: 'Success', content: `x: ${res.x}, y: ${res.y}, z: ${res.z}`});
+      // my.alert({title: 'Success', content: `x: ${res.x}, y: ${res.y}, z: ${res.z}`});
+      this.setData({data: res});
     };
     my.startAcceleronmeter({
       interval: 'ui',
