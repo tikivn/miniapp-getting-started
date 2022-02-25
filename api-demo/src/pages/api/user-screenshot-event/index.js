235c8cf621020c6d2601ea2cfd28040d921aea40
phamdang2810-tiki <dang.pham@tiki.vn>
2022-02-25 22:02:21 +0700

    update

--- api-demo/src/pages/api/user-screenshot-event/index.js
+++ api-demo/src/pages/api/user-screenshot-event/index.js
@@ -2,18 +2,20 @@ Page({
   data: {
     condition: false,
   },
+  onReady() {
+    this.callback = () => {
+      my.alert({
+        content: 'Receive a screenshot from the user' , 
+      });
+    };
+    my.onUserCaptureScreen(this.callback);
+  },
   onTapOnUserCaptureScreen() {
-    my.onUserCaptureScreen({
-      success: () => {
-        this.setData({condition: true});
-      },
-    });
+    my.onUserCaptureScreen();
+    this.setData({condition: true});
   },
   onTapOffUserCaptureScreen() {
-    my.offUserCaptureScreen({
-      success: () => {
-        this.setData({condition: false});
-      },
-    });
+    my.offUserCaptureScreen();
+    this.setData({condition: false});
   },
 });
