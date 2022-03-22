104e116ff99c036d5f53d2593c47075b42b0146d
phamdang2810-tiki <dang.pham@tiki.vn>
2022-03-22 11:12:05 +0700

    update content get cart

--- api-demo/src/pages/api/get-cart/index.js
+++ api-demo/src/pages/api/get-cart/index.js
@@ -1,10 +1,6 @@
 Page({
-  data: {
-    cart: null,
-  },
   onGetCart() {
     my.getCart({
-      openLogin: true,
       success: (res) => {
         console.log(res);
         my.alert({title: 'Total number of items', content: res.total});
