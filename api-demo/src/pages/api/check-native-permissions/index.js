3e50871c6adec66e7995f717dd696125378071f7
mai.nguyen-tiki <mai.nguyen6@tiki.vn>
2022-03-27 13:44:22 +0700

    fix: demo jsapi checkNativePermissions

--- api-demo/src/pages/api/check-native-permissions/index.js
+++ api-demo/src/pages/api/check-native-permissions/index.js
@@ -1,34 +1,14 @@
 Page({
   data: {
-    permissions: [
-      { key: 'contacts', value: false },
-      { key: 'location', value: false },
-      { key: 'notification', value: false },
-      { key: 'camera', value: false },
-      { key: 'photo', value: false },
-    ],
+    scope:[],
+    permissions: ['contacts', 'location', 'notification', 'camera','photo'],
   },
   onChange(e) {
-    if (e.detail.value.length > 0) {
-      const index = e.detail.value[0];
-      this.data.permissions[index] = {
-        ...this.data.permissions[index],
-        value: !this.data.permissions[index].value,
-      };
-      this.setData({
-        permissions: this.data.permissions,
-      });
-    }
+    this.setData({scope: e.detail.value});
   },
   onCheckPermissions() {
-    const scope = this.data.permissions.reduce((arr, item) => {
-      if (item.value) {
-        arr.push(item.key);
-      }
-      return arr;
-    }, []);
     my.checkNativePermissions({
-      scope,
+      scope: this.data.scope,
       success: (res) => {
         my.alert({ content: JSON.stringify(res) });
       },
