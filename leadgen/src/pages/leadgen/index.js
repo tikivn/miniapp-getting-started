49948110b58f0c3fa4b046b1d15837fc5fd732e3
hungnguyentiki <hung.nguyen7@tiki.vn>
2022-08-22 11:03:39 +0700

    feat: update app leadgen

--- leadgen/src/pages/leadgen/index.js
+++ leadgen/src/pages/leadgen/index.js
@@ -5,4 +5,12 @@ Page({
     const { id } = qs.parse(query);
     this.setData({ id });
   },
+  onSubmitSuccess() {
+    my.showToast({
+      type: 'success',
+      content: 'Hồ sơ mua được tạo thành công',
+      duration: 3000,
+    });
+    my.navigateBack();
+  },
 });
